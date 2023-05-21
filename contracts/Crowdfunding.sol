a645a06b8f2eda626228037cb06af9212229b7f0
faisalfh5 <faisalh550@gmail.com>
2023-05-21 21:50:08 +0500

    built smart contract by hardhat

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <=0.9.0;
 
-
-
 contract FundMe   {
     mapping(uint256 => Campaign) public campaigns;
     mapping(address => uint) public investors;
